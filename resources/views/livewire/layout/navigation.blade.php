1a5e3332bd9a299b91a5fc6d94ecb15d027993b7
Pramod2960 <pramodkumar2960@gmail.com>
2026-01-20 13:05:04 +0530

    newq

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -31,6 +31,7 @@ new class extends Component {
                 @endhasanyrole
 
                 <!-- Navigation Links -->
+                @hasanyrole('superadmin')
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
@@ -41,6 +42,7 @@ new class extends Component {
                         Tasks
                     </x-nav-link>
                 </div>
+                @endhasanyrole
 
                 @hasanyrole('superadmin')
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
@@ -120,11 +122,14 @@ new class extends Component {
 
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
+
+        @hasanyrole('superadmin')
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
+        @endhasanyrole
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
